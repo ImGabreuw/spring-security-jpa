47d51e38877fd51d96d1cd3b47709531e447aa28
ImGabreuw <imgabreuw.dev@gmail.com>
2021-03-10 15:51:24 -0300

    Projeto criado

---
+++ src/main/java/me/gabreuw/spring_security_jpa/MainApplication.java
@@ -0,0 +1,13 @@
+package me.gabreuw.spring_security_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainApplication.class, args);
+	}
+
+}
